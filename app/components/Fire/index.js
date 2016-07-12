7d4f91b3e47bd90cb1b2be244a877099fa99ebbc
Annie Bae <bae.annie@gmail.com>
2016-07-12 10:48:19 -0400

    Link acquired to navigate components

--- app/components/Fire/index.js
+++ app/components/Fire/index.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router'
 
 class Fire extends Component {
 	render() {
 		return(
-			<div>Set fire to da rain</div>
+			<div>Set fire to da rain
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/hello">Hello</Link>
+					</li>
+					<li>
+						<Link to="/fire">Fire</Link>
+					</li>
+				</ul>
+			</div>
 		)
 	}
 }
--- app/components/Hello/index.js
+++ app/components/Hello/index.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router'
 
 class Hello extends Component {
 	render() {
 		return (
-			<div> Hello holla hellurrr from the other side </div>
+			<div> Hello holla hellurrr from the other side
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/hello">Hello</Link>
+					</li>
+					<li>
+						<Link to="/fire">Fire</Link>
+					</li>
+				</ul>
+			</div>
 		)
 	}
 }
