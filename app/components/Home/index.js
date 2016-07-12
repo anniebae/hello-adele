f07f60c1d7906a01190d2871a2a394c811ef10fa
Annie Bae <bae.annie@gmail.com>
2016-07-12 11:37:44 -0400

    installing npm style dependencies

--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router'
-import Menu from 'components/Menu';
+import Styles from './styles.css'
+import Menu from 'components/Menu'
 
 class Home extends Component {
 	render() {
--- app/index.js
+++ app/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import {Route, Router, IndexRoute, browserHistory} from 'react-router'
 
-import Home from 'components/Home';
-import Hello from 'components/Hello';
-import Fire from 'components/Fire';
+import Home from 'components/Home'
+import Hello from 'components/Hello'
+import Fire from 'components/Fire'
 
 
 const App = props => {
